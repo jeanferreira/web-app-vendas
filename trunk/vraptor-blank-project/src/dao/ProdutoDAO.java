cb98b2b2b08deb9e5eff8a9c97fdfa48e14cd922
jeansiufu@gmail.com <jeansiufu@gmail.com@c3ea93ef-7861-6f5c-75f8-c8c32c0c0fec>
2014-03-27 02:08:02 +0000

    Cadastro de Produtos com Formulario

--- trunk/vraptor-blank-project/src/dao/ProdutoDAO.java
+++ trunk/vraptor-blank-project/src/dao/ProdutoDAO.java
@@ -1,25 +1,33 @@
 package dao;
+
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
 
 import br.com.caelum.vraptor.ioc.Component;
 import model.*;
 
+@SuppressWarnings("deprecation")
 @Component
 public class ProdutoDAO {
 
-	
-	
 	@SuppressWarnings("unchecked")
 	public List<Produto> listar() {
 		return FabricaSessao.getSession().createCriteria(Produto.class).list();
 	}
-	
+
 	public void insere(Produto p) {
-		Transaction tx = FabricaSessao.getSession().beginTransaction();
-		FabricaSessao.getSession().save(p);
+		AnnotationConfiguration conf = new AnnotationConfiguration();
+		conf.configure();
+		SessionFactory f = conf.buildSessionFactory();
+		Session s = f.openSession();
+
+		Transaction tx = s.beginTransaction();
+		s.save(p);
 		tx.commit();
-		
+
 	}
 }
