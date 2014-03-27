a6f86e39ee2442c130ee0087a9fe39bc475e75b6
jeansiufu@gmail.com <jeansiufu@gmail.com@c3ea93ef-7861-6f5c-75f8-c8c32c0c0fec>
2014-03-27 14:33:03 +0000

    Funcionalidade Login implementada - Validação de Usuario

--- trunk/vraptor-blank-project/src/controller/ProdutosController.java
+++ trunk/vraptor-blank-project/src/controller/ProdutosController.java
@@ -3,6 +3,7 @@ package controller;
 import java.util.List;
 
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
 import model.Produto;
 import dao.ProdutoDAO;
 
@@ -10,9 +11,11 @@ import dao.ProdutoDAO;
 public class ProdutosController {
 
 	private final ProdutoDAO prodDAO;
+	private final Result result;
 
-	public ProdutosController(ProdutoDAO prodDAO) {
+	public ProdutosController(ProdutoDAO prodDAO, Result result) {
 		this.prodDAO = prodDAO;
+		this.result = result;
 	}
 
 	
@@ -22,6 +25,7 @@ public class ProdutosController {
 	
 	public void insereProduto(Produto p) {
 		this.prodDAO.insere(p);
+		result.redirectTo(ProdutosController.class).lista();
 	}
 	
 	public void formulario() {
--- trunk/vraptor-blank-project/src/dao/UsuarioDAO.java
+++ trunk/vraptor-blank-project/src/dao/UsuarioDAO.java
@@ -1,10 +1,33 @@
 package dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Usuario;
+
+import br.com.caelum.vraptor.ioc.Component;
+
+@Component
 public class UsuarioDAO {
-	
+
 	public void addUsuario() {
-		
-		
+
+	}
+
+	@SuppressWarnings({ "unused", "unchecked" })
+	public boolean connect(String login, String pass) {
+		boolean token = false;
+		List<Usuario> lista = new ArrayList<Usuario>();
+		lista = FabricaSessao.getSession().createCriteria(Usuario.class).list();
+
+		for (Usuario u : lista) {
+			if (lista.equals(login) && lista.equals(pass)) {
+				;
+			token = true;
+			} 
+		}
+
+		return token;
 	}
 
 }
