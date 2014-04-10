a32062fbbf0609b559d1648a608c9d04fd8322dd
jeansiufu@gmail.com <jeansiufu@gmail.com@c3ea93ef-7861-6f5c-75f8-c8c32c0c0fec>
2014-04-10 17:21:51 +0000

    lista cliente

--- trunk/vraptor-blank-project/src/dao/ClienteDAO.java
+++ trunk/vraptor-blank-project/src/dao/ClienteDAO.java
@@ -1,10 +1,20 @@
 package dao;
 
 
+import java.util.List;
+
+import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Restrictions;
 import org.hibernate.Session;
+
+import sun.misc.Cleaner;
+
+import com.sun.org.apache.regexp.internal.recompile;
+
 import model.Cliente;
 import br.com.caelum.vraptor.ioc.Component;
 
@@ -14,9 +24,12 @@ import br.com.caelum.vraptor.ioc.Component;
 public class ClienteDAO {
 	
 	private final FabricaSessao fabrica;
+	private List<Cliente> lista;
 	
-	public ClienteDAO(FabricaSessao fabrica) {
+	public ClienteDAO(FabricaSessao fabrica, List<Cliente> lista) {
 		this.fabrica = fabrica;
+		this.lista = lista;
+		
 	}
 	
 	@SuppressWarnings("static-access")
@@ -25,6 +38,15 @@ public class ClienteDAO {
 		Transaction tx = s.beginTransaction();
 		s.save(c);
 		tx.commit();
+	}
+	
+	@SuppressWarnings({ "static-access", "unchecked" })
+	public List<Cliente> lista(String nome) {
+		Criteria crit = fabrica.getSession().createCriteria(Cliente.class);
+		crit.add(Restrictions.ilike(nome, MatchMode.ANYWHERE));
+		lista = crit.list();
+		
+		return lista;
 	}
 
 }
